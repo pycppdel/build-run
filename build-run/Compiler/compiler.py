29308310b015d53ba6331761dbb2ee26aff35ab3
root <pycppdel@gmail.com>
2021-09-30 23:50:42 +0200

    autosave

---
+++ build-run/Compiler/compiler.py
@@ -0,0 +1,6 @@
+"""
+Class Compiler compiles the given Code into an Dictionary, that will be given to
+Pythonizer Class.
+
+
+"""
---
+++ build-run/Compiler/pythonizer.py
@@ -0,0 +1,5 @@
+"""
+File with a class that generates python code from given Dictionary of Data.
+
+Loads Libraries automatically
+"""
